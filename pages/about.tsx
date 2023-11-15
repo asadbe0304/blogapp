ab8820c9975226f2b2b422b3c0d89921eb492d97
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-11-15 09:58:02 +0500

    Initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <div>About</div>
+    </>
+  );
+};
+
+export default about;
